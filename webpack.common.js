69baba51bc84e81ba70fd1580f1f9220c392ed02
shermanhui <11592023+shermanhui@users.noreply.github.com>
2022-04-08 17:47:38 -0700

    fix: copy manifest and images to dist
    Add copy-webpack-plugin to copy
    the manifest and images to the dist folder.
    The contents of `dist` can be uploaded as
    an unpacked extension on Chrome when
    developer mode is enabled.

--- webpack.common.js
+++ webpack.common.js
@@ -1,40 +1,67 @@
 const path = require("path");
+const copyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-      background: path.join(__dirname, "src/background/index.ts")
+    background: path.join(__dirname, "src/background/index.ts"),
     // popup: path.join(__dirname, "src/popup/index.tsx"),
     // eventPage: path.join(__dirname, "src/eventPage.ts")
   },
   output: {
-    path: path.join(__dirname, "dist/retro-clippy"),
-    filename: "[name].js"
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
   },
   module: {
     rules: [
       {
         exclude: /node_modules/,
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: "ts-loader",
       },
-    //   {
-    //     exclude: /node_modules/,
-    //     test: /\.scss$/,
-    //     use: [
-    //       {
-    //         loader: "style-loader" // Creates style nodes from JS strings
-    //       },
-    //       {
-    //         loader: "css-loader" // Translates CSS into CommonJS
-    //       },
-    //       {
-    //         loader: "sass-loader" // Compiles Sass to CSS
-    //       }
-    //     ]
-    //   }
-    ]
+      //   {
+      //     exclude: /node_modules/,
+      //     test: /\.scss$/,
+      //     use: [
+      //       {
+      //         loader: "style-loader" // Creates style nodes from JS strings
+      //       },
+      //       {
+      //         loader: "css-loader" // Translates CSS into CommonJS
+      //       },
+      //       {
+      //         loader: "sass-loader" // Compiles Sass to CSS
+      //       }
+      //     ]
+      //   }
+    ],
   },
+  plugins: [
+    new copyPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, "src/manifest.json"),
+          to: path.join(__dirname, "dist"),
+          force: true,
+        },
+        {
+          from: path.join(__dirname, "src/static/icons/clip_16.png"),
+          to: path.join(__dirname, "dist"),
+          force: true,
+        },
+        {
+          from: path.join(__dirname, "src/static/icons/clip_48.png"),
+          to: path.join(__dirname, "dist"),
+          force: true,
+        },
+        {
+          from: path.join(__dirname, "src/static/icons/clip_128.png"),
+          to: path.join(__dirname, "dist"),
+          force: true,
+        },
+      ],
+    }),
+  ],
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  }
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
